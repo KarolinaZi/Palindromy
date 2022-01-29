deb80c19fe64a1979204818f80b388ba570add87
Karolina Ziejlo <karolinaziejlo@wp.pl>
2022-01-29 21:43:35 +0100

    Add corrected version-reversed words compared

--- Palindromy.py
+++ Palindromy.py
@@ -6,24 +6,17 @@
 # Znasz już funkcje kolekcji, które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
 #Do zadania dodaj krótką dokumentację i umieść je w zdalnym repozytorium. Link prześlij Mentorowi.
 
-
 def czy_palindrom(word):
     """
     Checks if a word is a palindrome.
     """
-    lenght = len(word)
-    if lenght % 2 != 0:
-        lenght = lenght+1
-    iterations_no = int(lenght/2)
+    if word == word[::-1]:
+        result = True
+    else:
+        result = False
     
-    iloczyn = 1
-    for i in range(0,iterations_no):
-        if word[0+i] == word [-1-i]:
-            result = 1
-        else:
-            result = 0
-        iloczyn = iloczyn*result
-    print(bool(iloczyn))
-   
+    print(result)
 
 czy_palindrom("anna")
+
+
